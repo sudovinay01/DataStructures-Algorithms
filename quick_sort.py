8179287988ddba88a4992dae9262f4cff8f80481
Siddhi Chaithanya <32200253+AumaujayaSiddhi@users.noreply.github.com>
2024-09-15 11:30:51 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -14,6 +14,7 @@ import numpy as np
 class QuickSort:
   """
       Class contains different operations problems on quick sort algorithm.
+      Author: Siddhi Chaithanya
   """
   def __init__(self, original_array):
     """
