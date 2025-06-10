18887d3ad7e8d3d37db09254ee144d1c69b97357
Siddhi Chaithanya <32200253+AumaujayaSiddhi@users.noreply.github.com>
2025-06-10 22:59:30 +0530

    Added recursive insertion

--- trees/bst.py
+++ trees/bst.py
@@ -1,17 +1,11 @@
 from nodes.node import Node_V3
+from algorithms_base.options_error import OptionNotFound
+import re
 class BST:
     def __init__(self):
         self.root = None
-    
-    def _insert(self, data):
-        """
-        BST insertion
-        Time Complexity : O(n), θ(logn)
-        Space Complexity : O(1)
-        """
-        if not self.root:
-            self.root = Node_V3(data)
-            return
+
+    def __insert_iteration(self, data):
         
         ant, parent_ant = self.root, None
         while ant:
@@ -32,8 +26,38 @@ class BST:
             new_ant = Node_V3(data)
             new_ant.left = ant
             parent_ant.left = new_ant 
+    
+    def __insert_recursion(self, item, data):
+        if data <= item.data:
+            if not item.left:
+                item.left = Node_V3(data)
+            else:
+                self.__insert_recursion(item.left, data)
+        elif data > item.data:
+            if not item.right:
+                item.right = Node_V3(data)
+            else:
+                self.__insert_recursion(item.right, data)
+
+    def _insert(self, data, how="recursion"):
+        """
+        BST insertion
+        Time Complexity : O(n), θ(logn)
+        Space Complexity : O(1)
+        """
+        if not self.root:
+            self.root = Node_V3(data)
+            return
+
+        if re.fullmatch(r"(iteration|i)", how, re.IGNORECASE):
+            return self.__insert_iteration(data)
+        if re.fullmatch(r"(recursion|r)", how, re.IGNORECASE):
+            return self.__insert_recursion(self.root, data)
 
     def __delete_key(self, p, c):
+        """
+        Support code for __delete_iteration method
+        """
         # If key is a leaf node
         if not c.left and not c.right:
             if not p:
@@ -74,12 +98,7 @@ class BST:
             p.data = c.data
             memory_ant.left = None if not c.right else c.right
     
-    def _delete(self, key):
-        """
-        BST deletion
-        Time Complexity : O(n), θ(logn)
-        Space Complexity : O(1)
-        """
+    def __delete_iteration(self, key):
         if not self.root:
             print(f"BST is empty")
             return
@@ -99,6 +118,21 @@ class BST:
         
         # If key not found
         print(f"{key} not found")
+
+    def __delete_recursion(self, item, key):
+        print(f"Deletion using recursion is not yet implemented please use iteration...")
+        return
+    
+    def _delete(self, key, how="iteration"):
+        """
+        BST deletion
+        Time Complexity : O(n), θ(logn)
+        Space Complexity : O(1)
+        """
+        if re.fullmatch(r"(iteration|i)", how, re.IGNORECASE):
+            return self.__delete_iteration(key)
+        if re.fullmatch(r"(recursion|r)", how, re.IGNORECASE):
+            return self.__delete_recursion(self.root, key) 
     
     def __inorder_traversal(self, p):
         """
@@ -139,11 +173,21 @@ class BST:
         Time Complexity : O(n)
         Space Complexity : O(1)
         """
-        print(f"Current BST : ")
-        if how == "inorder":
-            self.__inorder_traversal(self.root)
-        elif how == "preorder":
-            self.__preorder_traversal(self.root)
-        else:
-            self.__postorder_traversal(self.root)
-        print()
+        if not self.root:
+            print(f"BST is empty...")
+            return
+        
+        try:
+            print(f"Current BST : ")
+            if re.fullmatch(r"(inorder|in)", how, re.IGNORECASE):
+                self.__inorder_traversal(self.root)
+            elif re.fullmatch(r"(preorder|pre)", how, re.IGNORECASE):
+                self.__preorder_traversal(self.root)
+            elif re.fullmatch(r"(postorder|post)", how, re.IGNORECASE):
+                self.__postorder_traversal(self.root)
+            else:
+                raise OptionNotFound(f"Not a right choice please correct the how = {how}")
+        except OptionNotFound as onf:
+            print(onf)
+        finally:
+            print()
