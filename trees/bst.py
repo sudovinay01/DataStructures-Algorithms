d5a136cb2eb2359dcd2f4d1e04a1c9a73989d4c1
Siddhi Chaithanya <32200253+AumaujayaSiddhi@users.noreply.github.com>
2025-06-10 22:07:49 +0530

    Added BST deletion

--- trees/bst.py
+++ trees/bst.py
@@ -4,7 +4,11 @@ class BST:
         self.root = None
     
     def _insert(self, data):
-        
+        """
+        BST insertion
+        Time Complexity : O(n), θ(logn)
+        Space Complexity : O(1)
+        """
         if not self.root:
             self.root = Node_V3(data)
             return
@@ -28,43 +32,113 @@ class BST:
             new_ant = Node_V3(data)
             new_ant.left = ant
             parent_ant.left = new_ant 
+
+    def __delete_key(self, p, c):
+        # If key is a leaf node
+        if not c.left and not c.right:
+            if not p:
+                self.root = None
+            elif p.left == c:
+                p.left = None
+            else:
+                p.right = None
+            return
+        
+        # If ant.right is None that means we donot need to find
+        # the element that needs to be replaced with the node 
+        # that needs to be deleted
+        if not c.right:
+            if not p:
+                self.root = c.left
+            elif p.left == c:
+                p.left = c.left
+            else:
+                p.right = c.left
+            return
+
+        # If ant.right is not None then the code comes to this part
+        # Now the element that needs to be replaced with the data that
+        # needs to be deleted needs to be found out
+        p = c
+        c = c.right
+        memory_ant = None
+        while c.left:
+            memory_ant = c
+            c = c.left
+
+        # Replacing the element that needs to be deleted with the right element
+        if not memory_ant:
+            p.data = c.data
+            p.right = c.right
+        else:
+            p.data = c.data
+            memory_ant.left = None if not c.right else c.right
     
     def _delete(self, key):
-        """if not self.root:
+        """
+        BST deletion
+        Time Complexity : O(n), θ(logn)
+        Space Complexity : O(1)
+        """
+        if not self.root:
             print(f"BST is empty")
             return
         
-        parent_ant, ant = None, self.root
+        # Finding the key that need to be deleted...
+        parent_ant = None
+        ant = self.root
         while ant:
-            parent_ant = ant
             if key < ant.data:
+                parent_ant = ant
                 ant = ant.left
             elif key > ant.data:
+                parent_ant = ant
                 ant = ant.right
             else:
-                pass"""
-        print("Not yet implemented")
-        return
+                return self.__delete_key(parent_ant, ant)
+        
+        # If key not found
+        print(f"{key} not found")
     
     def __inorder_traversal(self, p):
+        """
+        BST Inorder traversal
+        Time Complexity : O(n)
+        Space Complexity : O(1)
+        """
         if p:
             self.__inorder_traversal(p.left)
             print(p.data, end=",")
             self.__inorder_traversal(p.right)
 
     def __preorder_traversal(self, p):
+        """
+        BST Preorder traversal
+        Time Complexity : O(n)
+        Space Complexity : O(1)
+        """
         if p:
             print(p.data, end=",")
             self.__preorder_traversal(p.left)
             self.__preorder_traversal(p.right)
     
     def __postorder_traversal(self, p):
+        """
+        BST Post traversal
+        Time Complexity : O(n)
+        Space Complexity : O(1)
+        """
         if p:
             self.__postorder_traversal(p.left)
             self.__postorder_traversal(p.right)
             print(p.data, end=",")
     
-    def show_bst(self, how="inorder"):
+    def _show_bst(self, how="inorder"):
+        """
+        BST traversals
+        Time Complexity : O(n)
+        Space Complexity : O(1)
+        """
         print(f"Current BST : ")
         if how == "inorder":
             self.__inorder_traversal(self.root)
@@ -72,3 +146,4 @@ class BST:
             self.__preorder_traversal(self.root)
         else:
             self.__postorder_traversal(self.root)
+        print()
